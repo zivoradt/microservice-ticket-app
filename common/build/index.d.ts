7c4561c9ae337b6a45f3e579edd70f6e102a5baf
zivoradt <zivoradtrickovic@gmail.com>
2023-03-24 15:56:33 +0100

    npm package 1.0.3v published

--- common/build/index.d.ts
+++ common/build/index.d.ts
@@ -1,7 +1,10 @@
-interface Color {
-    red: number;
-    blue: number;
-}
-declare const color: Color;
-
-export default Color;
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-auth-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
+export * from './middelwares/current-user';
+export * from './middelwares/error-handler';
+export * from './middelwares/require-auth';
+export * from './middelwares/validate-request';
--- common/src/index.ts
+++ common/src/index.ts
@@ -1,11 +1,14 @@
-interface Color{
-    red: number,
-    blue: number
-}
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-auth-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
 
-const color: Color = {
-    red: 10,
-    blue: 10
-}
+export * from './middelwares/current-user';
+export * from './middelwares/error-handler';
+export * from './middelwares/require-auth';
+export * from './middelwares/validate-request';
 
-console.log(color);
+
+
